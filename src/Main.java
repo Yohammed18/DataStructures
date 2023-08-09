e991388a184bb109a5e0742be07f7fab9ca88b61
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-08-08 21:45:20 -0400

    Binary Search

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package src;
+
+import src.search.BinarySearch;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearch bs = new BinarySearch();
+
+        //testing the application
+        //creating an sorted array
+        int[] sortedArray = { 22, 44, 51, 59, 64, 67, 68, 81, 91, 94, 114, 132, 144, 148, 149, 154, 163, 181, 196, 197};
+        System.out.println(bs.binarySearch(sortedArray,44));
+        //The binary is a Log2n
+        //log of 1is always 0
+        //ask the base if youre not sure
+        //doubling the size of a list will equate to log2n= x+1
+        //time complexity (how long it will take it)
+        //space complexity (how much memory we will need)
+        //n represents the number of inputs
+        //the growth of the number of operation will determine how fast the operation is. Not measured directly in secs
+
+    }
+}
---
+++ src/search/BinarySearch.java
@@ -0,0 +1,43 @@
+package src.search;
+/**
+ * @author malli
+ * @implSpec Binary class with a binary search method that will find the number in an array in the fastest way possible using the Big O notation of Log2n
+ */
+public class BinarySearch {
+
+
+    public int binarySearch(int[] numbers, int findNumber){
+
+        //utilize two pointers
+        int low = 0;//this pointer will track the begging of the arrays index
+        int high = numbers.length - 1;//this will track the last index of the array
+
+        //this pointer will track the number of steps it took to find the position of the array
+        int count = 1;
+
+
+        while(low <= high){
+            //first will create a variable that will store the middle position of the array. We will obtain that value by getting the average number
+            int middlePosition = (low+high)/2;
+            //second we will get the actual number that's located in the middle of the array by the provided middle position
+            int middleNumber = numbers[middlePosition];
+
+            //Lastly we will establish the cases that will determine if the number is actually part of the array by using a conditional statement
+
+            if(findNumber == middleNumber){
+                //if the findNUmber equals to the middleNumber the condition will return the index where the number is located in the array
+                System.out.println("The number of steps it took to find the number: "+count);
+                return middlePosition;
+            } else if(findNumber < middleNumber){
+                //since this is a sorted array, if the find number is less than the middle number we are going to set the high value to equal the middle position minus 1
+                high = middlePosition -1;
+            } else{
+                //this will focus on setting the low number to a new value. Since by default  the middle number is greater than then findNumber the low pointer value will equal to the middle numberNumber position + 1
+                low = middlePosition + 1;
+            }
+            count++;
+        }
+        //the application will return the -1 if the findNumber value is not part of the array
+        return -1;
+    }
+}
