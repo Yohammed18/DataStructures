effe74df19b5c4b27c324beb5cb8559de7a137fa
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-01-11 22:04:53 -0500

    Update complete

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
 package src;
 
-import src.search.Stack;
+/**
+ * @author malli
+ * @apiNote Arrays & Big-O Notation
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Stack stack = new Stack(5);
 
-        System.out.println(stack.isEmpty());
-
-        stack.push(3);
-        stack.push(23);
-        stack.push(45);
-        stack.push(63);
-        stack.push(-1);
-        System.out.println(stack.peak());
-
-        stack.show();
-        System.out.println(stack.pop());
-        stack.show();
-        System.out.println(stack.peak());
-
-        System.out.println(stack.isEmpty());
 
     }
 }
---
+++ src/arrays/Main.java
@@ -0,0 +1,28 @@
+package src.arrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //Arrays
+        int[] array = new int[7];
+        array[0] = 20;
+        array[1] = 35;
+        array[2] = -15;
+        array[3] = 7;
+        array[4] = 55;
+        array[5] = 1;
+        array[6] = -22;
+
+
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            index = 1;
+            break;
+        }
+
+        System.out.println("Index = "+index);
+    }
+}
